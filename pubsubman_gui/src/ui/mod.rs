83941c082e0e21f7ec0042fe3966181dae266841
dmackdev <dmackdev@gmail.com>
2023-08-04 16:10:25 +0100

    Renamed file.

--- pubsubman_gui/src/ui/mod.rs
+++ pubsubman_gui/src/ui/mod.rs
@@ -1,7 +1,7 @@
-mod publish_panel;
+mod publish_view;
 mod topic_name;
 mod topic_view;
 
-pub use publish_panel::PublishMessageFormState;
+pub use publish_view::PublishMessageFormState;
 pub use topic_name::render_topic_name;
 pub use topic_view::TopicViewState;
