0e5d445f7488777e8e1c0b062662ff48adf321ed
dmackdev <dmackdev@gmail.com>
2023-08-06 18:23:35 -0400

    Confirm quit modal styling.

--- pubsubman_gui/src/exit_state.rs
+++ pubsubman_gui/src/exit_state.rs
@@ -15,6 +15,13 @@ pub enum SubscriptionCleanupState {
     Complete,
 }
 
+const MARGIN: egui::Margin = egui::Margin {
+    left: 16.0,
+    right: 16.0,
+    top: 12.0,
+    bottom: 4.0,
+};
+
 impl ExitState {
     pub fn show(
         &mut self,
@@ -26,25 +33,48 @@ impl ExitState {
             return;
         }
 
-        Modal::new("exit_modal", "Confirm Exit").show(ctx, |ui| {
+        Modal::new("exit_modal", "Confirm Quit").show(ctx, |ui| {
             match self.subscription_cleanup_state {
                 SubscriptionCleanupState::Idle => {
-                    ui.label("Clean up Subscriptions?");
+                    egui::Frame::none().inner_margin(MARGIN).show(ui, |ui| {
+                        ui.allocate_ui_with_layout(
+                            egui::vec2(350.0, 150.0),
+                            egui::Layout::top_down(egui::Align::Center),
+                            |ui| {
+                                ui.label(
+                                    "Pubsubman created Subscriptions in order to receive messages. Do you want to delete these Subscriptions before you quit?",
+                                );
 
-                    ui.horizontal(|ui| {
-                        if ui.button("Yes").clicked() {
-                            cleanup_subscriptions();
-                            self.subscription_cleanup_state = SubscriptionCleanupState::Waiting;
-                        }
+                                ui.add_space(20.0);
 
-                        if ui.button("No").clicked() {
-                            self.can_exit = true;
-                            frame.close();
-                        }
+                                ui.horizontal(|ui| {
+                                    ui.with_layout(
+                                        egui::Layout::left_to_right(egui::Align::Center),
+                                        |ui| {
+                                            if ui.button("Delete Subscriptions").clicked() {
+                                                cleanup_subscriptions();
+                                                self.subscription_cleanup_state =
+                                                    SubscriptionCleanupState::Waiting;
+                                            }
 
-                        if ui.button("Cancel").clicked() {
-                            self.show_exit_dialogue = false;
-                        }
+                                            if ui.button("Skip").clicked() {
+                                                self.can_exit = true;
+                                                frame.close();
+                                            }
+
+                                            ui.with_layout(
+                                                egui::Layout::right_to_left(egui::Align::Center),
+                                                |ui| {
+                                                    if ui.button("Cancel").clicked() {
+                                                        self.show_exit_dialogue = false;
+                                                    }
+                                                },
+                                            );
+                                        },
+                                    );
+                                });
+                            },
+                        );
                     });
                 }
                 SubscriptionCleanupState::Waiting => {
