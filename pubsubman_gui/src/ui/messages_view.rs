fa549c5b67f784e164ed92cd8ce2f2552c626e59
dmackdev <dmackdev@gmail.com>
2023-08-06 08:35:15 +0100

    Showing search bar and column settings on messages view.

--- pubsubman_gui/src/ui/messages_view.rs
+++ pubsubman_gui/src/ui/messages_view.rs
@@ -15,6 +15,8 @@ use crate::actions::{pull_message_batch, stream_messages};
 pub struct MessagesView {
     pub stream_messages_enabled: bool,
     pub stream_messages_cancel_token: Option<CancellationToken>,
+    pub search_query: String,
+    pub columns_settings: ColumnsSettings,
 }
 
 impl MessagesView {
@@ -75,7 +77,7 @@ impl MessagesView {
             .frame(
                 egui::Frame::central_panel(ui.style())
                     .fill(ui.style().visuals.extreme_bg_color)
-                    .inner_margin(16.0),
+                    .inner_margin(egui::vec2(16.0, 12.0)),
             )
             .show_inside(ui, |ui| {
                 if messages.is_empty() {
@@ -85,12 +87,36 @@ impl MessagesView {
                         ui.label("Pull or Stream new messages to retrieve the latest.");
                     });
                 } else {
+                    ui.horizontal(|ui| {
+                        ui.visuals_mut().extreme_bg_color = egui::Color32::from_gray(32);
+                        ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
+                            ui.add(
+                                egui::TextEdit::singleline(&mut self.search_query)
+                                    .desired_width(125.0)
+                                    .hint_text("Search"),
+                            );
+                            ui.with_layout(
+                                egui::Layout::right_to_left(egui::Align::Center),
+                                |ui| {
+                                    self.columns_settings.show(ui);
+                                },
+                            );
+                        });
+                    });
+
+                    let outer_margin = egui::Margin {
+                        top: 8.0,
+                        bottom: 12.0,
+                        ..Default::default()
+                    };
+
                     egui::Frame::none()
                         .fill(ui.style().visuals.panel_fill)
-                        .inner_margin(egui::Vec2::new(6.0, 3.0))
+                        .inner_margin(egui::vec2(6.0, 3.0))
+                        .outer_margin(outer_margin)
                         .rounding(ui.style().visuals.window_rounding)
                         .show(ui, |ui| {
-                            render_messages_table(ui, messages);
+                            render_messages_table(ui, &self.columns_settings, messages);
                         });
                 }
             });
@@ -99,36 +125,57 @@ impl MessagesView {
 
 const ROW_HEIGHT_PADDING: f32 = 4.0;
 
-fn render_messages_table(ui: &mut egui::Ui, messages: &[PubsubMessage]) {
+fn render_messages_table(
+    ui: &mut egui::Ui,
+    columns_settings: &ColumnsSettings,
+    messages: &[PubsubMessage],
+) {
     let text_height = ui.text_style_height(&egui::TextStyle::Monospace);
 
-    let table = TableBuilder::new(ui)
+    let mut table = TableBuilder::new(ui)
         .striped(true)
         .resizable(true)
         .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-        .column(Column::auto())
-        .column(Column::auto())
-        .column(Column::auto())
-        .column(Column::remainder())
         .min_scrolled_height(0.0)
         .auto_shrink([false, true]);
 
+    let ColumnsSettings {
+        show_id,
+        show_published_at,
+        show_attributes,
+    } = *columns_settings;
+
+    for col_enabled in [show_id, show_published_at, show_attributes] {
+        if col_enabled {
+            table = table.column(Column::auto());
+        }
+    }
+
+    // For Data column which cannot be hidden
+    table = table.column(Column::remainder());
+
     table
         .header(20.0, |mut header| {
-            header.col(|ui| {
-                ui.label("ID");
-            });
+            if show_id {
+                header.col(|ui| {
+                    ui.label("ID");
+                });
+            }
+
+            if show_published_at {
+                header.col(|ui| {
+                    ui.label("Published at");
+                });
+            }
+
+            if show_attributes {
+                header.col(|ui| {
+                    ui.label("Attributes");
+                });
+            }
 
             header.col(|ui| {
-                ui.label("Published at");
-            });
-
-            header.col(|ui| {
-                ui.label("Attributes");
-            });
-
-            header.col(|ui| {
-                ui.label("Payload");
+                ui.label("Data");
             });
         })
         .body(|mut body| {
@@ -137,21 +184,30 @@ fn render_messages_table(ui: &mut egui::Ui, messages: &[PubsubMessage]) {
                 let row_height = num_lines as f32 * text_height + ROW_HEIGHT_PADDING;
 
                 body.row(row_height, |mut row| {
-                    row.col(|ui| {
-                        ui.label(&message.id);
-                    });
+                    if show_id {
+                        row.col(|ui| {
+                            ui.label(&message.id);
+                        });
+                    }
 
-                    row.col(|ui| {
-                        if let Some(publish_time) = message.publish_time {
-                            let local_publish_time: DateTime<Local> = publish_time.into();
+                    if show_published_at {
+                        row.col(|ui| {
+                            if let Some(publish_time) = message.publish_time {
+                                let local_publish_time: DateTime<Local> = publish_time.into();
 
-                            ui.label(format!("{}", local_publish_time.format("%d/%m/%Y %H:%M")));
-                        }
-                    });
+                                ui.label(format!(
+                                    "{}",
+                                    local_publish_time.format("%d/%m/%Y %H:%M")
+                                ));
+                            }
+                        });
+                    }
 
-                    row.col(|ui| {
-                        ui.label(format_attributes(&message.attributes));
-                    });
+                    if show_attributes {
+                        row.col(|ui| {
+                            ui.label(format_attributes(&message.attributes));
+                        });
+                    }
 
                     row.col(|ui| {
                         ui.label(&message.data);
@@ -174,4 +230,39 @@ fn format_attributes(attributes: &HashMap<String, String>) -> String {
             )
         })
         .collect()
+}
+
+pub struct ColumnsSettings {
+    show_id: bool,
+    show_published_at: bool,
+    show_attributes: bool,
+}
+
+impl Default for ColumnsSettings {
+    fn default() -> Self {
+        Self {
+            show_id: true,
+            show_published_at: true,
+            show_attributes: true,
+        }
+    }
+}
+
+impl ColumnsSettings {
+    fn show(&mut self, ui: &mut egui::Ui) {
+        ui.visuals_mut().widgets.inactive.weak_bg_fill = egui::Color32::from_gray(32);
+        ui.menu_button("Columns ⏷", |ui| {
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut self.show_id, " ID");
+            });
+
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut self.show_published_at, " Published at");
+            });
+
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut self.show_attributes, " Attributes");
+            });
+        });
+    }
 }
