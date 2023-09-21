a361c9db49329e73cbf4efa06536437ac7847a05
dmackdev <dmackdev@gmail.com>
2023-09-21 19:20:59 +0100

    Extracted backend methods.

--- pubsubman_backend/src/lib.rs
+++ pubsubman_backend/src/lib.rs
@@ -7,12 +7,13 @@ use google_cloud_pubsub::{
     subscription::SubscriptionConfig,
 };
 use message::{BackendMessage, FrontendMessage};
-use model::{SubscriptionName, TopicName};
+use model::{PubsubMessageToPublish, SubscriptionName, TopicName};
 use tokio::{
     runtime::{Builder, Runtime},
     select,
     sync::mpsc::{Receiver, Sender},
 };
+use tokio_util::sync::CancellationToken;
 use uuid::Uuid;
 
 pub mod message;
@@ -51,119 +52,143 @@ impl Backend {
     }
 
     pub fn init(&mut self) {
-        let rt = &self.rt;
-
         while let Some(message) = self.front_rx.blocking_recv() {
             match message {
                 FrontendMessage::RefreshTopicsRequest => {
-                    let back_tx = self.back_tx.clone();
-                    let client = self.client.clone();
-                    rt.spawn(async move {
-                        let topics = client
-                            .get_topics(None)
-                            .await
-                            .unwrap()
-                            .into_iter()
-                            .map(TopicName)
-                            .collect();
-
-                        back_tx
-                            .send(BackendMessage::TopicsUpdated(topics))
-                            .await
-                            .unwrap();
-                    });
+                    self.get_topics();
                 }
                 FrontendMessage::CreateSubscriptionRequest(topic_name) => {
-                    let back_tx = self.back_tx.clone();
-                    let client = self.client.clone();
-
-                    rt.spawn(async move {
-                        let sub_name = format!("pubsubman-subscription-{}", Uuid::new_v4());
-
-                        let _subscription = client
-                            .create_subscription(
-                                &sub_name,
-                                &topic_name.0,
-                                SubscriptionConfig::default(),
-                                None,
-                            )
-                            .await
-                            .unwrap();
-
-                        back_tx
-                            .send(BackendMessage::SubscriptionCreated(
-                                topic_name,
-                                SubscriptionName(sub_name),
-                            ))
-                            .await
-                            .unwrap();
-                    });
+                    self.create_subscription(topic_name);
                 }
                 FrontendMessage::DeleteSubscriptions(sub_names) => {
-                    let back_tx = self.back_tx.clone();
-                    let client = self.client.clone();
-
-                    rt.spawn(async move {
-                        let futures = sub_names.into_iter().map(|sub_name| {
-                            let client = client.clone();
-                            async move {
-                                let subscription = client.subscription(&sub_name.0);
-                                match subscription.delete(None).await {
-                                    Ok(_) => Ok(sub_name),
-                                    Err(_) => Err(sub_name),
-                                }
-                            }
-                        });
-
-                        let results = futures::future::join_all(futures).await;
-
-                        back_tx
-                            .send(BackendMessage::SubscriptionsDeleted(results))
-                            .await
-                            .unwrap();
-                    });
+                    self.delete_subscriptions(sub_names);
                 }
                 FrontendMessage::StreamMessages(topic_name, sub_name, cancel_token) => {
-                    let back_tx = self.back_tx.clone();
-                    let client = self.client.clone();
-
-                    rt.spawn(async move {
-                        let subscription = client.subscription(&sub_name.0);
-
-                        let pull_messages_future = async move {
-                            let mut stream = subscription.subscribe(None).await.unwrap();
-
-                            while let Some(message) = stream.next().await {
-                                let _ = message.ack().await;
-
-                                back_tx
-                                    .send(BackendMessage::MessageReceived(
-                                        topic_name.clone(),
-                                        message.into(),
-                                    ))
-                                    .await
-                                    .unwrap();
-                            }
-                        };
-
-                        select! {
-                          _ = cancel_token.cancelled() => {}
-                          _ = pull_messages_future => {}
-                        }
-                    });
+                    self.stream_messages(topic_name, sub_name, cancel_token);
                 }
                 FrontendMessage::PublishMessage(topic_name, message) => {
-                    let client = self.client.clone();
-
-                    rt.spawn(async move {
-                        let topic = client.topic(&topic_name.0);
-                        let publisher = topic.new_publisher(None);
-                        let awaiter = publisher.publish(message.into()).await;
-                        awaiter.get().await
-                    });
+                    self.publish_message(topic_name, message);
                 }
             }
         }
+    }
+
+    fn get_topics(&self) {
+        let back_tx = self.back_tx.clone();
+        let client = self.client.clone();
+
+        self.rt.spawn(async move {
+            let topics = client
+                .get_topics(None)
+                .await
+                .unwrap()
+                .into_iter()
+                .map(TopicName)
+                .collect();
+
+            back_tx
+                .send(BackendMessage::TopicsUpdated(topics))
+                .await
+                .unwrap();
+        });
+    }
+
+    fn create_subscription(&self, topic_name: TopicName) {
+        let back_tx = self.back_tx.clone();
+        let client = self.client.clone();
+
+        self.rt.spawn(async move {
+            let sub_name = format!("pubsubman-subscription-{}", Uuid::new_v4());
+
+            let _subscription = client
+                .create_subscription(
+                    &sub_name,
+                    &topic_name.0,
+                    SubscriptionConfig::default(),
+                    None,
+                )
+                .await
+                .unwrap();
+
+            back_tx
+                .send(BackendMessage::SubscriptionCreated(
+                    topic_name,
+                    SubscriptionName(sub_name),
+                ))
+                .await
+                .unwrap();
+        });
+    }
+
+    fn delete_subscriptions(&self, sub_names: Vec<SubscriptionName>) {
+        let back_tx = self.back_tx.clone();
+        let client = self.client.clone();
+
+        self.rt.spawn(async move {
+            let futures = sub_names.into_iter().map(|sub_name| {
+                let client = client.clone();
+                async move {
+                    let subscription = client.subscription(&sub_name.0);
+                    match subscription.delete(None).await {
+                        Ok(_) => Ok(sub_name),
+                        Err(_) => Err(sub_name),
+                    }
+                }
+            });
+
+            let results = futures::future::join_all(futures).await;
+
+            back_tx
+                .send(BackendMessage::SubscriptionsDeleted(results))
+                .await
+                .unwrap();
+        });
+    }
+
+    fn stream_messages(
+        &self,
+        topic_name: TopicName,
+        sub_name: SubscriptionName,
+        cancel_token: CancellationToken,
+    ) {
+        let back_tx = self.back_tx.clone();
+        let client = self.client.clone();
+
+        self.rt.spawn(async move {
+            let subscription = client.subscription(&sub_name.0);
+
+            let pull_messages_future = async move {
+                let mut stream = subscription.subscribe(None).await.unwrap();
+
+                while let Some(message) = stream.next().await {
+                    let _ = message.ack().await;
+
+                    back_tx
+                        .send(BackendMessage::MessageReceived(
+                            topic_name.clone(),
+                            message.into(),
+                        ))
+                        .await
+                        .unwrap();
+                }
+            };
+
+            select! {
+              _ = cancel_token.cancelled() => {}
+              _ = pull_messages_future => {}
+            }
+        });
+    }
+
+    fn publish_message(&self, topic_name: TopicName, message: PubsubMessageToPublish) {
+        let client = self.client.clone();
+
+        self.rt.spawn(async move {
+            let topic = client.topic(&topic_name.0);
+            let publisher = topic.new_publisher(None);
+            let awaiter = publisher.publish(message.into()).await;
+            awaiter.get().await
+        });
     }
 }
 
